e21f32214c71d32542552c30e7be55341d3e02ff
mzazakeith <mzazakeith@gmail.com>
2021-10-21 14:15:25 +0300

    simple get student method without database

--- src/main/java/com/springboot/demo/SpringbootDemoApplication.java
+++ src/main/java/com/springboot/demo/SpringbootDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 //This annotation has springbootconfiguration+ enable auto configuration + component scan annotations
-//component scan annotation scans all spring components within base package plus all sub packages 
+//component scan annotation scans all spring components within base package plus all sub packages
 //add @component to classes to create spring component
 //We can also use @configuration in a java class and use spring based configurations in the normal class
 public class SpringbootDemoApplication {
