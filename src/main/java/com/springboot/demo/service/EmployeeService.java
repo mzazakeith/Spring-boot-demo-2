476874d6e62669ac3f2bc62a61bc1008fed1553c
mzazakeith <mzazakeith@gmail.com>
2021-10-22 13:06:29 +0300

    added jpa and started custom esception

---
+++ src/main/java/com/springboot/demo/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.springboot.demo.service;
+
+import com.springboot.demo.model.Employee;
+
+public interface EmployeeService {
+
+    }
+}
---
+++ src/main/java/com/springboot/demo/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,11 @@
+package com.springboot.demo.service.impl;
+
+import com.springboot.demo.model.Employee;
+import com.springboot.demo.repository.EmployeeRepository;
+import com.springboot.demo.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+}
