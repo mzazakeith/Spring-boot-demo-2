93c87bf5563dc84df1b93d687d413c70fd837b01
mzazakeith <mzazakeith@gmail.com>
2021-10-25 17:45:10 +0300

    corrections

--- src/main/java/com/springboot/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/demo/exception/GlobalExceptionHandler.java
@@ -2,13 +2,13 @@ package com.springboot.demo.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 //    handle specific exceptions
     @ExceptionHandler(ResourceNotFoundException.class)
