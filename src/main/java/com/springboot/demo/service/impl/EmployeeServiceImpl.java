85e630c4250ad3bf3c4502794f777c2e2ec749e9
mzazakeith <mzazakeith@gmail.com>
2021-10-25 13:38:19 +0300

    change exception name to resouce not found

--- src/main/java/com/springboot/demo/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/springboot/demo/service/impl/EmployeeServiceImpl.java
@@ -1,13 +1,12 @@
 package com.springboot.demo.service.impl;
 
-import com.springboot.demo.exception.RNotFoundException;
+import com.springboot.demo.exception.ResourceNotFoundException;
 import com.springboot.demo.model.Employee;
 import com.springboot.demo.repository.EmployeeRepository;
 import com.springboot.demo.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 //no need to add transaction annotation spring data jpa will provide transaction to all its methods
@@ -37,15 +36,15 @@ public class EmployeeServiceImpl implements EmployeeService {
 //        if(employee.isPresent()){
 //            return employee.get();
 //        }else{
-//            throw new RNotFoundException("Employee", "Id", id);
+//            throw new ResourceNotFoundException("Employee", "Id", id);
 //        }
-        return employeeRepository.findById(id).orElseThrow(() -> new RNotFoundException("Employee", "Id",id));
+        return employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee", "Id",id));
     }
 
     @Override
     public Employee updateEmployee(Employee employee, long id) {
 //        check whether employee with given id is existing database or not
-        Employee existingEmployee = employeeRepository.findById(id).orElseThrow(() ->new RNotFoundException("Employee", "Id",id));
+        Employee existingEmployee = employeeRepository.findById(id).orElseThrow(() ->new ResourceNotFoundException("Employee", "Id",id));
         existingEmployee.setFirstName(employee.getFirstName());
         existingEmployee.setLastName(employee.getLastName());
         existingEmployee.setEmail(employee.getEmail());
@@ -57,7 +56,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     public void deleteEmployee(long id) {
 
 //        check whether employee exists or not
-        employeeRepository.findById(id).orElseThrow(() ->new RNotFoundException("Employee", "Id",id));
+        employeeRepository.findById(id).orElseThrow(() ->new ResourceNotFoundException("Employee", "Id",id));
         employeeRepository.deleteById(id);
 
     }
